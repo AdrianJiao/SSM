a6d7c55bd1038dd052d9e42c79ed94d1b577d6b8
asker012 <3066566138@qq.com>
2018-04-10 09:34:34 +0800

    用户名更改的测试

--- src/com/company/filter/EncodingFilter.java
+++ src/com/company/filter/EncodingFilter.java
@@ -9,6 +9,7 @@ public class EncodingFilter implements Filter {
     public void destroy() {
     }
 
+//    添加另一行注释，拉链式的封口拉到沙发上空了的房间
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         req.setCharacterEncoding("utf-8");
         resp.setContentType("text/html;charset=utf-8");
